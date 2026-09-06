9684ed8665f3661f6f49323b4bada085246d78fc
kevin-117 <krodriuezcondori34@gmail.com>
2026-09-05 22:30:01 -0400

    feat: add test api

---
+++ test-api/request.py
@@ -0,0 +1,12 @@
+import requests
+import numpy as np
+
+# Crear datos sintéticos de prueba con forma (30, 1662)
+dummy_data = np.zeros((30, 1662)).tolist()
+
+# Enviar petición POST a FastAPI
+url = "http://127.0.0.1:8000/predict"
+payload = {"sequence": dummy_data}
+
+response = requests.post(url, json=payload)
+print(response.json())
